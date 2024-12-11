a00ce55e8aac2d5b72a56a142483b84502aad39f
LevCha2132272 <charles.levike@gmail.com>
2024-12-11 05:40:48 -0500

    3
    3

--- Web/Site/app/Views/pages/scores.php
+++ Web/Site/app/Views/pages/scores.php
@@ -10,7 +10,33 @@
    height:80vh;
 
 }
+.content2{
+    justify-content:center;
+    align-items:center;
 
+}
+.lb{
+    display:flex;
+    flex-direction:column;
+    justify-content:center;
+    align-items:center;
+    background-color:gray;
+    padding:2vh;
+    border-radius:5vh;
+}
+h2,h3{
+    margin:0.1vh;
+  
+}
+
+.h3{
+    display:flex;
+   justify-content:center;
+   align-items:center;
+   background-color:lightgray;
+    border-radius:0.5vh;
+    width:10vh;
+}
 </style>
 <div class = "page">
 <div class = "content">
@@ -41,29 +67,32 @@ $arrayUsers = array();
  
     }
     echo "<div class = 'lb'>";
-    echo "<h1>SCORE LEADERBOARD<BR>";
-    echo "USER, SCORE</h1><BR>";
+    echo "<h1>SCORE LEADERBOARD";
 
         for ($i = 0; $i< count($arrayUsers);$i++){
 
-            echo "<h3>USERNAME: ".$arrayUsers[$i]->username;
-            echo "  SCORE:".$arrayScore[$i]->score."</h3>";
+            echo "<h2>USERNAME: </h2><div class = 'h3'><h3>".$arrayUsers[$i]->username."</h3></div>";
+            echo "<h2>SCORE: </h2><div class = 'h3'><h3>".$arrayScore[$i]->score."</h3></div>";
         }
 
         
         $session = session();
 
         $query  = $db->query("SELECT * FROM score where id_user = '$session->id'LIMIT 1" );
-        $results = $query->getResult();
+        $results = $query->getResult();?>
+     
+        <?php 
+        
         foreach ($results as $row)
         {
-            echo"<h3>Your Best Score: ".$row->score . "<br>up input".$row->up_input."<br>down input".$row->down_input."<br>left Input". $row->left_input. "<br>right input".
-            $row->right_input. "<br>pressed input".$row->pressed_input."</h3>";
+            echo"<h2>Your Best Score: </h2><div class = 'h3'><h3>".$row->score . "</h3></div><div class = 'content2'>up input: <div class = 'h3'><h3>".$row->up_input."</h3></div>down input: <div class = 'h3'><h3>".$row->down_input."</h3></div>left Input: <div class = 'h3'><h3>". $row->left_input. "</h3></div>right input: <div class = 'h3'><h3>".
+            $row->right_input. "</h3></div>pressed input: <div class = 'h3'><h3>".$row->pressed_input."</h3></div>";
         }
 
 
         
 ?>
+  </div>
     </div>
     </div>
     </div>
